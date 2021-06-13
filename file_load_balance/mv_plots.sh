7a9d98b36683de9d3435e82f9e9158c45bf67710
Alpha725 <48570690+Alpha725@users.noreply.github.com>
2021-06-13 17:13:27 +1000

    Update mv_plots.sh
    Way better, tested on two systems and it load balances perfectly

--- file_load_balance/mv_plots.sh
+++ file_load_balance/mv_plots.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-Directory=""
-plot_count=0
-temp_plots=$(ls /mnt/SSD | grep '.plots$'| wc -l)
-if [ $temp_plots -gt 0 ]
-	then
-		for plots in `ls /mnt/SSD`
-		do
-			for i in `seq 24`
-			do
-				plots=$(ls /mnt/server/drive-$i/BBC-farm/ | wc -l)
-				if [ $plot_count -eq 0 ]
-				then
-					plot_count=$(ls /mnt/server/drive-$i/BBC-farm/ | wc -l)
-				fi
-				if [ $plots -le $plot_count ]
-				then
-					Directory=$i
-					numb_plots=$(ls /mnt/server/drive-$i/BBC-farm/ | wc -l)
-				fi
-			done
-			[ $(ps aux | grep 'mv ' | grep -v grep | wc -l) -eq 0 ] && mv /mnt/SSD/*.plot /mnt/server/drive-$Directory/BBC-farm/
-			echo "`date`: plot moved to drive-$Directory plot: $plots"
-		done
-	else
-		echo "`date`: No plots" >> /opt/load_balance/mv_log.txt
-fi
+#for plot in `ls /mnt/SSD | grep .plot$`
+#do
+
+function get_drive() {
+
+        drive_count=$(ls /mnt/server/drive-1/BBC-farm/ | grep .plot$ | wc -l)
+        drive_number=0
+
+        for i in `seq 24`
+        do
+                count=$(ls /mnt/server/drive-$i/BBC-farm/ | grep .plot$ | wc -l)
+                if [ $count -le $drive_count ]
+                then
+                        drive_number=$i
+                fi
+        done
+}
+
+for plot in `ls /mnt/SSD | grep .plot$`
+do
+        echo "`date`: found plot: $plot" >> /opt/load_balance/mv_log.txt
+        get_drive
+        echo "`date`: moving to: /mnt/server/drive-$drive_number/BBC-farm/" >> /opt/load_balance/mv_log.txt
+        [ $(ps aux | grep 'mv ' | grep -v grep | wc -l) -eq 0 ] && mv /mnt/SSD/$plot /mnt/server/drive-$drive_number/BBC-farm/
+        echo "`date`: finished moving plot: $plot" >> /opt/load_balance/mv_log.txt
+done
